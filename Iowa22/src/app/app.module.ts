51fe72a257f173f91f4237bac6fbf29fa9534146
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-07-05 17:02:43 -0500

    nested comp

--- Iowa22/src/app/app.module.ts
+++ Iowa22/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoadTripComponent } from './road-trip/road-trip.component';
+import { HeaderComponent } from './header/header.component';
+import { InsideRoadTripComponent } from './road-trip/inside-road-trip/inside-road-trip.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoadTripComponent
+    RoadTripComponent,
+    HeaderComponent,
+    InsideRoadTripComponent
   ],
   imports: [
     BrowserModule,
